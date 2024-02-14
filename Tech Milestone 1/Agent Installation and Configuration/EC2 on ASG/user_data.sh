3a4e4afe2f108c17d420fae79e6a554e4fa3ecbc
jon94 <jonathan.lim@datadoghq.com>
2024-02-14 17:23:15 +0800

    EC2 on ASG

---
+++ Tech Milestone 1/Agent Installation and Configuration/EC2 on ASG/user_data.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export DATADOG_API_KEY=XXXXXXX
+export DD_ENV=<set the env here>
+export DD_SERVICE=<set service here>
+
+DD_API_KEY=$DATADOG_API_KEY \
+bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
+
+sudo -u dd-agent -g dd-agent touch /etc/datadog-agent/environment
+sudo chown $(whoami) /etc/datadog-agent/environment
+cat <<EOT > /etc/datadog-agent/environment
+DD_SERVICE=$DD_SERVICE 
+DD_ENV=$DD_ENV 
+DD_APM_ENABLED=true 
+DD_APM_NON_LOCAL_TRAFFIC=true 
+EOT
+sudo chown dd-agent /etc/datadog-agent/environment
+
+sudo chown $(whoami) /etc/datadog-agent/datadog.yaml
+sudo cat <<EOT > /etc/datadog-agent/datadog.yaml
+api_key: $DATADOG_API_KEY
+process_config:
+  process_collection:
+    enabled: true
+EOT
+sudo chown dd-agent /etc/datadog-agent/datadog.yaml
+
+# Restart the Datadog Agent
+sudo systemctl restart datadog-agent
+
+# Get Datadog Agent Status
+sudo systemctl status datadog-agent
